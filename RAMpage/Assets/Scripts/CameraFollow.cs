30e1a75b38989fb55844b836f9ed182122250fca
Blake <blakeivorshea@gmail.com>
2025-03-28 21:28:18 -0400

    Add Ram Horizontal Input

---
+++ RAMpage/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    [SerializeField] private Camera _camera;
+    [SerializeField] Transform target;
+    [SerializeField] float lerpX, lerpY;
+    [SerializeField] float positionX, positionY, positionZ;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = target.position;
+        positionX = target.position.x;
+        positionY = target.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target) Follow();
+    }
+
+    void Follow()
+    {
+        positionX = Mathf.Lerp(transform.position.x, target.position.x, lerpX);
+        positionY = Mathf.Lerp(transform.position.y, target.position.y, lerpY);
+        transform.position = new Vector3(positionX, positionY, positionZ);
+    }
+}
---
+++ RAMpage/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float forward, backward;
+    private Vector2 forwardVec, backwardVec;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        forwardVec = new Vector2(forward, 0);
+        backwardVec = new Vector2(backward, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) rb.AddForce(backwardVec); else rb.AddForce(forwardVec);
+    }
+}
