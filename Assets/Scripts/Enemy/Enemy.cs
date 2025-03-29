e1a9215a48bbb34c1a30f667cfe9243a7901d739
CHEESECAKE98 <miller.ben7@yahoo.com>
2025-03-29 17:00:35 -0400

    Enemy Attack

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 
 public class Enemy : MonoBehaviour
@@ -11,8 +12,10 @@ public class Enemy : MonoBehaviour
     [SerializeField] int Reward;
     [SerializeField] Collider2D Hitbox;
     [SerializeField] float speed;
+    [SerializeField] float AttackRadius;
     private GameObject Player;
     public EnemyObject Type;
+    [SerializeField] bool CanAttack;
 
     public void Start()
     {
@@ -48,10 +51,29 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    public void attack()
+    {
+        if (CanAttack)
+        {
+            CanAttack = false;
+            StartCoroutine(AttackCooldown());
+            Debug.Log("Attacked");
+        }
+    }
+
     private void Update()
     {
         var step = speed * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, step);
+        if (Physics2D.OverlapCircle(transform.position, AttackRadius).TryGetComponent<PlayerMovement>(out PlayerMovement player))
+        {
+            attack();
+        }
     }
 
+    public IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        CanAttack = true;
+    }
 }
