c6c195ca3918bd9e08438948f4a72fb800d90ff7
Blake <blakeivorshea@gmail.com>
2025-03-28 23:13:32 -0400

    Correct Vertical Movement

--- RAMpage/Assets/Scripts/Player/PlayerMovement.cs
+++ RAMpage/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,20 +3,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
-    [SerializeField] float forward, backward;
-    private Vector2 forwardVec, backwardVec;
+    [SerializeField] float forwardForce, backwardForce, forwardVerticalY, backwardVerticalY;
+    float verticalY;
+    [SerializeField] float maxReverse, maxForward;
+    private Vector2 forwardForceVec, backwardForceVec;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        forwardVec = new Vector2(forward, 0);
-        backwardVec = new Vector2(backward, 0);
+        forwardForceVec = new Vector2(forwardForce, 0);
+        backwardForceVec = new Vector2(-backwardForce, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A)) rb.AddForce(backwardVec); else rb.AddForce(forwardVec);
         
+        if (Input.GetAxis("Horizontal") < 0) { // Reverse 
+            rb.AddForce(backwardForceVec);
+        } else rb.AddForce(forwardForceVec); // Accelerate
+        rb.linearVelocity = new Vector2(Mathf.Clamp(rb.linearVelocity.x, maxReverse, maxForward), rb.linearVelocity.y); //Max/min
+
+        // Can't move up/down as fast is backing up
+        if (rb.linearVelocity.y > 1) verticalY = forwardVerticalY; else verticalY = backwardVerticalY;
+
+        // Move up/down
+        if (Input.GetAxis("Vertical") < 0) transform.Translate(0, -verticalY * Time.deltaTime, 0);
+        else if (Input.GetAxis("Vertical") > 0) transform.Translate(0, verticalY * Time.deltaTime, 0);
+
     }
 }
